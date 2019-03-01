2d0e4f6d008f48ff4c9453889ec7d7b20a933f36
Samuel Guo <guosamuel1114@gmail.com>
2019-03-01 18:35:57 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,54 @@
+require "pry"
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    if arguments.is_a? String
+      length_array = []
+      length_array.push(arguments)
+      
+      if length_array.length == 1
+        array1 = []
+        self.each do |zoo, location|
+          if location == arguments
+            array1.push(zoo)
+          end
+        end
+
+      else
+        array1 = []
+        counter = 0
+        while counter < length_array.length
+          self.each do |key, value|
+            if value == arguments
+              array1.push(key)
+            end
+          end
+          counter += 1
+        end
+      end
+    array1
+    
+    elsif arguments.is_a? Fixnum
+      array2 = []
+      self.each do |key, value|
+        if value == arguments
+          array2.push(key)
+        end
+      end
+      array2
+      
+    # else
+    #   array3 = []
+    #   counter = 0
+    #   while counter < arguments.length
+    #     self.each do |key, value|
+    #       if value == arguments
+    #         array3.push(key)
+    #       end
+    #     end
+    #     counter += 1
+    #   end
+    #   array3
+    end  
   end
 end
